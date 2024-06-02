b81afd678f28ad427ad6f406bbc9101693b66667
Gustavo <gustavo0411@gmail.com>
2024-06-02 18:18:48 -0300

    Estamos no processo de transição de arquitetura live data

--- app/src/main/java/com/example/thecoin/view/CurrencyExchangeFragment.kt
+++ app/src/main/java/com/example/thecoin/view/CurrencyExchangeFragment.kt
@@ -6,12 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.AdapterView
+import android.widget.Spinner
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
+import androidx.lifecycle.MutableLiveData
 import com.example.thecoin.adapter.SpinnerAdapter
 import com.example.thecoin.databinding.FragmentCurrencyExchangeBinding
-import com.example.thecoin.repository.Repository
 import com.example.thecoin.viewmodel.CoinViewModel
 
 
@@ -20,7 +20,8 @@ class CurrencyExchangeFragment : Fragment() {
     private val binding get() = _binding!!
     val coinViewModel: CoinViewModel by viewModels()
     var spinnerAdapter: SpinnerAdapter? = null
-    var repository = Repository()
+    lateinit var spinnerOne : Spinner
+    lateinit var spinnerTwo: Spinner
 
 
     override fun onCreateView(
@@ -30,106 +31,87 @@ class CurrencyExchangeFragment : Fragment() {
 
         _binding =
             FragmentCurrencyExchangeBinding.inflate(
-                LayoutInflater.from(requireContext()),
+                inflater,
                 container,
                 false
             )
 
+        spinnerOne = binding.spinnerFirstCoin
+        spinnerTwo = binding.spinnerSecondCoin
 
         spinnerAdapter =
             SpinnerAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item)
 
-
-        test()
-
-
-        binding.button.setOnClickListener(){
-            repository.cambioQuery(
-                coinViewModel.firstCoinSelected,
-                coinViewModel.secondCoinSelected,
-                coinViewModel._queryResult)
-
-
-
-
-
-        }
-
-
-
-        binding.numberTest.text = coinViewModel.queryResult.toString()
-        Log.e("cambio", coinViewModel.queryResult.toString())
-
-
-
-        coinViewModel.queryResult.observe(viewLifecycleOwner, Observer { bidValue ->
-            Log.e("bidValue","${coinViewModel.queryResult}")
-
-
-
-
-
-
-
-
-            binding.numberTest.text = bidValue.toString()
-
-
-            Toast.makeText(requireContext(), "$bidValue", Toast.LENGTH_SHORT).show()
-
-
-        })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        initSelector()
+        initViewModel()
 
 
 
 
 
         return binding.root
-
-
     }
 
 
-    fun test() {
+    fun initSelector() {
 
-        coinViewModel.spinnerInit(
-            binding.coinOne,
+        initSpinner(
+            spinnerOne!!,
             spinnerAdapter!!,
-            1,
+            1, coinViewModel._positionCoinOne,
             coinViewModel._firstCoinSelected
         )
-        coinViewModel.spinnerInit(
-            binding.coinTwo,
+        initSpinner(
+            spinnerTwo!!,
             spinnerAdapter!!,
-            0,
+            0, coinViewModel._positionCoinTwo,
             coinViewModel._secondCoinSelected
         )
 
+    }
 
+    fun initSpinner(
+        spinner: Spinner,
+        spinnerAdapter: SpinnerAdapter,
+        initiCoinPosition: Int,
+        invertCoinPosition: MutableLiveData<Int>,
+        coinSelected: MutableLiveData<String>
+    ) {
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val item: String = parent?.getItemAtPosition(position).toString()
+                Log.i("atual",position.toString())
+                coinSelected.value = item
+                invertCoinPosition.value = position
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
+        spinner.adapter = spinnerAdapter
+        spinner.setSelection(initiCoinPosition)
+    }
+
+    private fun initViewModel() {
+        coinViewModel.firstCoinSelected.observe(viewLifecycleOwner) { firstCoinSelection ->
+            coinViewModel.queryCoin()
+            coinViewModel.convertCoin(binding.inputUser, viewLifecycleOwner)
+        }
+        coinViewModel.secondCoinSelected.observe(viewLifecycleOwner) { secondCoinSelection ->
+            coinViewModel.queryCoin()
+            coinViewModel.convertCoin(binding.inputUser, viewLifecycleOwner)
+        }
+        coinViewModel.queryResult.observe(viewLifecycleOwner) { queryResult ->
+            binding.numberTest.text = queryResult.toString()
+        }
+        binding.invertArrow.setOnClickListener(){
+            coinViewModel.invertCoin(spinnerOne,spinnerTwo)
+        }
 
 
     }
@@ -138,14 +120,17 @@ class CurrencyExchangeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        coinViewModel.coinConverted.observe(viewLifecycleOwner) { convertedCoin ->
+            binding.numberTest.text = convertedCoin.toString()
+        }
+
+
 
     }
 
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onDestroyView() {
+        super.onDestroyView()
         _binding = null
     }
-
-
 }
--- app/src/main/java/com/example/thecoin/viewmodel/CoinViewModel.kt
+++ app/src/main/java/com/example/thecoin/viewmodel/CoinViewModel.kt
@@ -1,75 +1,110 @@
 package com.example.thecoin.viewmodel
 
-import android.view.View
-import android.widget.AdapterView
+import android.icu.text.Transliterator.Position
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.Spinner
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.thecoin.adapter.SpinnerAdapter
 import com.example.thecoin.repository.Repository
+import java.util.Locale
 
 class CoinViewModel : ViewModel() {
 
     val _firstCoinSelected = MutableLiveData<String>()
 
-    val firstCoinSelected : LiveData<String> get() = _firstCoinSelected
+    val firstCoinSelected: LiveData<String> get() = _firstCoinSelected
 
     val _secondCoinSelected = MutableLiveData<String>()
 
-    val secondCoinSelected : LiveData<String> get() = _secondCoinSelected
+    val secondCoinSelected: LiveData<String> get() = _secondCoinSelected
 
-     val _queryResult = MutableLiveData<Double>()
+    val _queryResult = MutableLiveData<Double>()
 
     val repository = Repository()
 
-    val queryResult : LiveData<Double> get() = _queryResult
+    val queryResult: LiveData<Double> get() = _queryResult
 
 
+    val _coinConverted = MutableLiveData<Double>()
 
-     fun spinnerInit(
-        spinner: Spinner,
-        spinnerAdapter: SpinnerAdapter,
-        positionCoin: Int,
-        coinSelected:MutableLiveData<String>) {
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                val item: String = parent?.getItemAtPosition(position).toString()
-                coinSelected.value = item
-
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-
-            }
-        }
+    val coinConverted: LiveData<Double> get() = _coinConverted
 
 
-        spinner.adapter = spinnerAdapter
+    val _positionCoinOne = MutableLiveData<Int>()
 
-        spinner.setSelection(positionCoin)
+    val positionCoinTwo: LiveData<Int> get() = _positionCoinOne
 
+    val _positionCoinTwo = MutableLiveData<Int>()
 
+    val positionCoinOne: LiveData<Int> get() = _positionCoinTwo
 
+    fun queryCoin() {
 
-
+        repository.cambioQuery(
+            firstCoinSelected,
+            secondCoinSelected,
+            _queryResult
+        )
 
 
     }
 
-    fun convertCoin(){
+    fun invertCoin(
+        spinnerOne: Spinner,
+        spinnerTwo: Spinner,
+    ) {
+
+        spinnerOne.setSelection(positionCoinOne.value!!)
+        spinnerTwo.setSelection(positionCoinTwo.value!!)
 
 
     }
 
 
+    fun convertCoin(editText: EditText, viewLifecycleOwner: LifecycleOwner) {
+
+        editText.addTextChangedListener(
+            object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
 
 
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    queryResult.observe(viewLifecycleOwner) {
+                        val inputValue = editText.text.toString().toDoubleOrNull() ?: 0.0
+                        val conversionRate = queryResult.value ?: 1.0
+                        val formatedNumber =
+                            String.format(Locale.US, "%.2f", inputValue * conversionRate)
+                        _coinConverted.value = formatedNumber.toDoubleOrNull()
+
+                    }
+
+
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+
+                }
+
+
+            })
+
+
+    }
 
 
 }
+
+
+
